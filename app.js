8a082cf235fdcb03c97fa5a21624b564341fa2a3
malkhaleel88 <>
2021-05-05 12:51:19 +0000

    adding js

---
+++ app.js
@@ -0,0 +1,14 @@
+// This is my first websites and it is about Perfumes //
+
+alert ("Welcome to Perfumes World")
+
+var Brand = prompt('What is your favourite Brand ?')
+
+if(Brand === 'Christian Dior'){
+  document.write('<img src="https://s3images.coroflot.com/user_files/individual_files/723615_ss5mbpxpjmmcajmcrtxsihppn.jpg">')
+}else if(Brand === 'CAROLINA HERRERA'){
+  document.write('<img src="https://www.brodheim.pt/media/2470/imagens-marcas-logos-08.png">')
+}else if(Brand === 'HUGO BOSS'){
+  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiVTNQjQGDx6NKtsTR4AfF5YBG1DCjeWZDmI2USMqO17U3oRwum0D4PlTHM_xoCp5E8uc&usqp=CAU">')
+}
+confirm("Thank you") 
